b28a585ee8ad263bc3bc28f6d217f35bb8139d03
DAYE ෆ <77138279+ekye26@users.noreply.github.com>
2021-03-27 01:04:59 +0900

    Update Health_scroll.kt

--- app/src/main/java/com/example/mit/mainhealthcare/Health_scroll.kt
+++ app/src/main/java/com/example/mit/mainhealthcare/Health_scroll.kt
@@ -64,9 +64,9 @@ class Health_scroll : AppCompatActivity() {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        val jdbcURL = "jdbc:postgresql://192.168.0.4:5432/server"
+        val jdbcURL = "jdbc:postgresql://:5432/server"
         val username = "postgres"
-        val password = "150526"
+        val password = ""
 
         try {
             val connection = DriverManager.getConnection(jdbcURL, username, password)
